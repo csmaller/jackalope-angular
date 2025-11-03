820c4906499b236ad4cf53fb6437291a85796d5d
corey <csmaller@elumatherapy.com>
2025-11-03 13:22:35 -0700

    prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://jackalope-backend.us-west-2.elasticbeanstalk.com'
+  apiUrl: 'http://jackalope-backend.us-west-2.elasticbeanstalk.com',
 };
